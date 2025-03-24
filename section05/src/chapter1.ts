733de42ebc6ba4230305a756200971b72ad30626
kimseonhee126 <kimseonhee126@gmail.com>
2025-03-24 11:55:50 +0900

    feat: 인터페이스 확장 학습하기

---
+++ section05/src/chapter1.ts
@@ -0,0 +1,37 @@
+/**
+ * 인터페이스 확장
+ */
+interface Animal {
+    name: string;
+    color: string;
+}
+
+interface Dog extends Animal {
+    isBark: boolean;
+}
+
+const dog: Dog = {
+    name: "",
+    color: "",
+    isBark: true,
+};
+
+interface Cat extends Animal {
+    isScratch: boolean;
+}
+
+interface Chicken {
+    isFly: boolean;
+}
+
+interface DogCat extends Dog, Cat {
+
+}
+
+const dogCat: DogCat = {
+    name: "",
+    color: "",
+    isBark: true,
+    isScratch: true,
+}
+
