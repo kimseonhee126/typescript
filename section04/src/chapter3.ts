5af79efbc4837ac1ad40076d74f1ad531ef39c9e
kimseonhee126 <kimseonhee126@gmail.com>
2025-03-24 11:28:18 +0900

    feat: 사용자 정의 타입 가드 공부하기

--- section04/src/chapter3.ts
+++ section04/src/chapter3.ts
@@ -1,10 +1,12 @@
 /**
  * 함수 오버로딩
  */
+// 오버 시그니처
 function func(a: number): void;
 function func(a: number, b: number, c: number): void;
 
 // 실제 구현부
+// 구현 시그니처
 function func(a: number, b?: number, c?: number) {
     if (typeof b === 'number' && typeof c === 'number') {
         console.log(a + b + c);
